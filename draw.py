f4a1e7221c02020abbfb3d7a9fca3c1ca179ea2a
Valter <->
2020-10-30 17:09:41 +0300

    Minimizing code size. Try: 1

--- draw.py
+++ draw.py
@@ -1,56 +1,65 @@
 import numpy as np
 import pygame
-
+import tensorflow as tf  
  
 class Display(object):
     
-    def __init__(self, sizeBrX,sizeBrY, win):
-        self.sizeBr = [sizeBrX, sizeBrY]
-        self.brd = np.array([28,28])  
-        self.brd = np.zeros(self.brd) 
-        self.board_shape = np.shape(self.brd)[0]
-        self.sizeX = sizeBrX // self.board_shape
-        self.sizeY = sizeBrY // self.board_shape
+    def __init__(self, model_path, win):
+        self.img_size = [win.get_height(), win.get_height()] 
+        self.img = np.array([28,28])  
+        self.img = np.zeros(self.img) 
+        self.board_shape = np.shape(self.img)[0]
+        self.sizeX = win.get_height() // self.board_shape
+        self.sizeY = win.get_height() // self.board_shape
         self.win = win
+        self.model = tf.keras.models.load_model(model_path)
+            
+        self.model.summary()
         
         self.myfont = pygame.font.SysFont('Comic Sans MS', 30)   
          
     def draw_on_screen(self):  
-        """ drawing picture on window """
-        pygame.draw.rect(self.win,(0, 0, 0),(0, 0, 1200, 840 ) )
-        for i in range(0, np.shape(self.brd)[0]): 
-            for j in range(0, np.shape(self.brd)[1]):    
-                if self.brd[i][j] == 0 :
+        """ output picture on window """
+        pygame.draw.rect(self.win,(0, 0, 0),(0, 0, self.win.get_width(), self.win.get_height()))
+        for i in range(0, np.shape(self.img)[0]): 
+            for j in range(0, np.shape(self.img)[1]):    
+                if self.img[i][j] == 0 :
                     pygame.draw.rect(self.win,(22, 22, 22),(i*self.sizeX, j*self.sizeY, self.sizeX-5, self.sizeY-5)) 
                 else:
-                    pygame.draw.rect(self.win,(self.brd[i][j] * 255,self.brd[i][j] * 255,self.brd[i][j] * 255),(i*self.sizeX, j*self.sizeY, self.sizeX-5, self.sizeY-5))                     
+                    pygame.draw.rect(self.win,(self.img[i][j] * 255,self.img[i][j] * 255,self.img[i][j] * 255),(i*self.sizeX, j*self.sizeY, self.sizeX-5, self.sizeY-5))                     
 
 
-    def new_value(self, pos, val):
-        """ setting drawed values to board list """
-        if pos[0] < self.sizeBr[0] and pos[1] < self.sizeBr[1]:      # границы поля для рисования
-            self.brd[pos[0]//self.sizeX][pos[1]//self.sizeY] = val
+    def write_new_value(self, pos, val):
+        """ setting drawed values to img list """
+        if pos[0] < self.img_size[0] and pos[1] < self.img_size[1]:      # границы поля для рисования
+            self.img[pos[0]//self.sizeX][pos[1]//self.sizeY] = val
             if pos[0]//self.sizeX+1 < self.board_shape:  # проверка на выход за границы массива снизу
-                if self.brd[pos[0]//self.sizeX+1][pos[1]//self.sizeY] != 1:
-                    self.brd[pos[0]//self.sizeX+1][pos[1]//self.sizeY] = val * 0.5     
+                if self.img[pos[0]//self.sizeX+1][pos[1]//self.sizeY] != 1:
+                    self.img[pos[0]//self.sizeX+1][pos[1]//self.sizeY] = val * 0.5     
             if pos[1]//self.sizeY+1 < self.board_shape:  # проверка на выход за границы массива справа
-                if self.brd[pos[0]//self.sizeX][pos[1]//self.sizeY+1] != 1:
-                    self.brd[pos[0]//self.sizeX][pos[1]//self.sizeY+1] = val * 0.5  
+                if self.img[pos[0]//self.sizeX][pos[1]//self.sizeY+1] != 1:
+                    self.img[pos[0]//self.sizeX][pos[1]//self.sizeY+1] = val * 0.5  
             if not pos[1]//self.sizeY+1 == 1:  # проверка на выход за границы массива сверху
-                if self.brd[pos[0]//self.sizeX][pos[1]//self.sizeY-1] != 1:
-                    self.brd[pos[0]//self.sizeX][pos[1]//self.sizeY-1] = val * 0.5
+                if self.img[pos[0]//self.sizeX][pos[1]//self.sizeY-1] != 1:
+                    self.img[pos[0]//self.sizeX][pos[1]//self.sizeY-1] = val * 0.5
             if not pos[0]//self.sizeX+1 == 1:  # проверка на выход за границы массива слева
-                if self.brd[pos[0]//self.sizeX-1][pos[1]//self.sizeY] != 1:
-                    self.brd[pos[0]//self.sizeX-1][pos[1]//self.sizeY] = val * 0.5
+                if self.img[pos[0]//self.sizeX-1][pos[1]//self.sizeY] != 1:
+                    self.img[pos[0]//self.sizeX-1][pos[1]//self.sizeY] = val * 0.5
 
-    def draw_prediction(self, predictions):
+    def draw_prediction(self):
         """  prints prediction for each number on right side of window"""
+
+        predictions = self.get_prediction()
         for i in range(0,len(predictions[0])):
             numbers = self.myfont.render((f'{chr(48+i)} = {  predictions[0][i] }'  ), True, (200, 200, 200)) # string for predictions   
-            self.win.blit(numbers,( self.sizeBr[0] + 10, 30 * (i + 1)))
+            self.win.blit(numbers,( self.img_size[0] + 10, 30 * (i + 1)))
         max = np.argmax(predictions[0])
         best_value = self.myfont.render((f'{chr(48+max)} = {  predictions[0][max] }'  ), True, (80, 80, 220)) # highlighting best value
-        self.win.blit(best_value,( self.sizeBr[0] + 10, 30 * (max + 1)))
+        self.win.blit(best_value,( self.img_size[0] + 10, 30 * (max + 1)))
 
-    def return_value(self):
-        return np.reshape(self.brd, (1, 28, 28, 1))
+    def get_prediction(self):
+        input = self.get_img_values()
+        return self.model.predict(input)
+
+    def get_img_values(self):
+        return np.reshape(self.img, (1, 28, 28, 1))
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import draw 
 import pygame 
 import os
-
-import tensorflow as tf  
-
  
-def interface(model):
+def interface(model_path):
     pygame.init()
     pygame.font.init()
     pygame.display.set_caption("drawing")
@@ -14,29 +11,23 @@ def interface(model):
          
     win = pygame.display.set_mode((1200, 840))
 
-    DP = draw.Display(840, 840, win)  
+    DP = draw.Display(model_path, win)  
     DP.draw_on_screen() 
 
     while(run): 
         clock.tick(80) 
 
-        if pygame.mouse.get_pressed()[0]: # drawing with left click
+        if pygame.mouse.get_pressed()[0]:   # drawing with left click
+            DP.write_new_value(pygame.mouse.get_pos(), 1)
+            DP.draw_on_screen()  
+
+            DP.draw_prediction()   
+
+        if pygame.mouse.get_pressed()[2]:   # erasing with right click 
+            DP.write_new_value(pygame.mouse.get_pos(), 0) 
             DP.draw_on_screen() 
-            DP.new_value(pygame.mouse.get_pos(), 1)   
- 
-            prediction_drawed =  DP.return_value()
-
-            prediction = model.predict(prediction_drawed)  
-            DP.draw_prediction(prediction)   
-
-        if pygame.mouse.get_pressed()[2]:   # erasing with right click
-            DP.draw_on_screen() 
-            DP.new_value(pygame.mouse.get_pos(), 0) 
- 
-            prediction_drawed =  DP.return_value()
-
-            prediction = model.predict(prediction_drawed)  
-            DP.draw_prediction(prediction) 
+   
+            DP.draw_prediction() 
             
         if pygame.key.get_pressed()[pygame.K_ESCAPE]: 
             run = False
@@ -48,11 +39,10 @@ def interface(model):
     pygame.display.quit()
     pygame.quit()
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__":    
     path = os.path.join(os.getcwd(), 'model/my_model.h5')
     path = 'C:/Users/Valter/Desktop/tests/Keras/Digits_rework/model/my_model.h5'
-    model = tf.keras.models.load_model(path)
-    model.summary()
-
-    interface(model)
+    if os.path.exists(path):
+        interface(path)
+    else:
+        print("Check file existense or change path variable")
